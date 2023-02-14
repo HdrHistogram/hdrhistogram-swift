b14898d9c21acb8142c511d27cb03673a7d5a067
dimlio <122263440+dimlio@users.noreply.github.com>
2023-02-14 19:04:57 +0200

    remove dependency on SwiftPackageIndex/SPIManifest

--- Package.swift
+++ Package.swift
@@ -23,7 +23,6 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
-        .package(url: "https://github.com/SwiftPackageIndex/SPIManifest.git", from: "0.12.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
